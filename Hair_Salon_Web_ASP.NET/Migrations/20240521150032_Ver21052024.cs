99b2694d619365aeecaf9f642a7a12936ecb9b2e
VanLap123 <belap123ct52@gmail.com>
2024-05-22 09:30:42 +0700

    update

---
+++ Hair_Salon_Web_ASP.NET/Migrations/20240521150032_Ver21052024.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hair_Salon_Web_ASP.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ver21052024 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Hair_Salon_Web_ASP.NET/Models/Service.cs
+++ Hair_Salon_Web_ASP.NET/Models/Service.cs
@@ -8,11 +8,14 @@ namespace Hair_Salon_Web_ASP.NET.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ser_id { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage = "Price is required.")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Please enter a valid price.")]
         public string price { get; set; }
         [Required]
         public string name { get; set; }
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a positive integer.")]
         public int time_to_cut { get; set; }
         public string? ser_image { get; set; }
         [NotMapped]
